8f90001b6f2e662ad11b35a14bc1a67ccc0fda20
Aimee Gonell <aimee.gonell@gmail.com>
2021-04-25 17:52:07 +0200

    Cleaning Function File

--- intersections.py
+++ intersections.py
@@ -8,8 +8,3 @@ l3 = np.random.randint(1, 99, 100)
 
 def intersection(l1, l2, l3):
     return list(set(l1) & set(l2) & set(l3))
-    
-print(intersection(l1, l2, l3))
-print(l1)
-print(l2)
-print(l3)
