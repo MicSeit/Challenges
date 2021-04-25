567b6a3acd7a4c237a91c07de36aafa41c0b6580
Aimee Gonell <aimee.gonell@gmail.com>
2021-04-25 17:27:58 +0200

    Replacing with a Function

--- intersections.py
+++ intersections.py
@@ -3,9 +3,13 @@ import numpy as np
 np.random.seed(42)
 
 l1 = np.random.randint(1, 99, 100)
-l3 = np.random.randint(1, 99, 100)
 l2 = np.random.randint(1, 99, 100)
+l3 = np.random.randint(1, 99, 100)
 
-intersections = set(l1).intersection(l2).intersection(l3)
-
-print(intersections)
+def intersection(l1, l2, l3):
+    return list(set(l1) & set(l2) & set(l3))
+    
+print(intersection(l1, l2, l3))
+print(l1)
+print(l2)
+print(l3)
