3d4ed492f1f9b9e964186b14b4b70ea7da2d0cd4
MicSeit <mic.seit@gmail.com>
2021-04-30 00:00:38 +0300

    Update intersections.py

--- intersections.py
+++ intersections.py
@@ -7,4 +7,5 @@ l2 = np.random.randint(1, 99, 100)
 l3 = np.random.randint(1, 99, 100)
 
 def intersection(l1, l2, l3):
+    """ Returns a list with the common number of given set of numbers """
     return list(set(l1) & set(l2) & set(l3))
